d4542ed2415cf2adfcb4505c3228aa70a1d4725c
Daniele Salvatore Albano <danielealbano@users.noreply.github.com>
2020-07-18 20:10:28 +0200

    Blocking socket and error checking (#6)
    * Add a map between the ops ids and the ops names
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * Use a const to define the number of buffers to provide
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * Use blocking socket for the server, otherwise will get a CQE per iteration with an EGAIN error to restart the listen
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * In case of error, print out the op, the error code and flags (split in high & low)
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * Use the const BUFFERS_COUNT to decide how many buffers have to be allocated
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * Move the buffer group id outside the main
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * General improvements to error checking
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * On accept, start to read from the fd only if it's not an error (ie. when the socket server is initialised as NONBLOCK)
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * Fix compiler warning
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * Increase the number of entries of the queue to 2048, with 1000 clients in the worst case scenario will be necessary to accomodate 1 listen, 1000 read and 1000 provide bufs
    
    Signed-off-by: Daniele Salvatore Albano <d.albano@gmail.com>
    
    * Drop error logging
    
    * Drop additional error control code
    
    * Restore error reporting for PROV_BUF
    
    * Drops ops name and ops ids, they are not used anymore
    
    * * we don't need to define IORING_FEAT_FAST_POLL any more (it's in liburing)
    * some informative comments
    
    Co-authored-by: frevib <hdevries@fastmail.com>

--- io_uring_echo_server.c
+++ io_uring_echo_server.c
@@ -11,10 +11,10 @@
 
 #include "liburing.h"
 
-#define MAX_CONNECTIONS 4096
-#define BACKLOG 512
-#define MAX_MESSAGE_LEN 2048
-#define IORING_FEAT_FAST_POLL (1U << 5)
+#define MAX_CONNECTIONS     4096
+#define BACKLOG             512
+#define MAX_MESSAGE_LEN     2048
+#define BUFFERS_COUNT       MAX_CONNECTIONS
 
 void add_accept(struct io_uring *ring, int fd, struct sockaddr *client_addr, socklen_t *client_len, unsigned flags);
 void add_socket_read(struct io_uring *ring, int fd, unsigned gid, size_t size, unsigned flags);
@@ -34,7 +34,8 @@ typedef struct conn_info {
     __u16 bid;
 } conn_info;
 
-char bufs[MAX_CONNECTIONS][MAX_MESSAGE_LEN];
+char bufs[BUFFERS_COUNT][MAX_MESSAGE_LEN] = {0};
+int group_id = 1337;
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -48,7 +49,7 @@ int main(int argc, char *argv[]) {
     socklen_t client_len = sizeof(client_addr);
 
     // setup socket
-    int sock_listen_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    int sock_listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     const int val = 1;
     setsockopt(sock_listen_fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
@@ -73,7 +74,7 @@ int main(int argc, char *argv[]) {
     struct io_uring ring;
     memset(&params, 0, sizeof(params));
 
-    if (io_uring_queue_init_params(1024, &ring, &params) < 0) {
+    if (io_uring_queue_init_params(2048, &ring, &params) < 0) {
         perror("io_uring_init_failed...\n");
         exit(1);
     }
@@ -94,13 +95,11 @@ int main(int argc, char *argv[]) {
     free(probe);
 
     // register buffers for buffer selection
-    unsigned buf_amount = 1000;
-    unsigned group_id = 1337;
     struct io_uring_sqe *sqe;
     struct io_uring_cqe *cqe;
 
     sqe = io_uring_get_sqe(&ring);
-    io_uring_prep_provide_buffers(sqe, bufs, MAX_MESSAGE_LEN, buf_amount, group_id, 0);
+    io_uring_prep_provide_buffers(sqe, bufs, MAX_MESSAGE_LEN, BUFFERS_COUNT, group_id, 0);
 
     io_uring_submit(&ring);
     io_uring_wait_cqe(&ring, &cqe);
@@ -128,7 +127,8 @@ int main(int argc, char *argv[]) {
 
             int type = conn_i.type;
             if (cqe->res == -ENOBUFS) {
-                printf("bufs in automatic buffer selection empty, this should not happen...\n");
+                fprintf(stdout, "bufs in automatic buffer selection empty, this should not happen...\n");
+                fflush(stdout);
                 exit(1);
             } else if (type == PROV_BUF) {
                 if (cqe->res < 0) {
@@ -137,14 +137,17 @@ int main(int argc, char *argv[]) {
                 }
             } else if (type == ACCEPT) {
                 int sock_conn_fd = cqe->res;
+                // only read when there is no error, >= 0
+                if (sock_conn_fd >= 0) {
+                    add_socket_read(&ring, sock_conn_fd, group_id, MAX_MESSAGE_LEN, IOSQE_BUFFER_SELECT);
+                }
 
                 // new connected client; read data from socket and re-add accept to monitor for new connections
-                add_socket_read(&ring, sock_conn_fd, group_id, MAX_MESSAGE_LEN, IOSQE_BUFFER_SELECT);
                 add_accept(&ring, sock_listen_fd, (struct sockaddr *)&client_addr, &client_len, 0);
             } else if (type == READ) {
                 int bytes_read = cqe->res;
-                if (bytes_read <= 0) {
-                    // no bytes available on socket, client must be disconnected
+                if (cqe->res <= 0) {
+                    // connection closed or error
                     shutdown(conn_i.fd, SHUT_RDWR);
                 } else {
                     // bytes have been read into bufs, now add write to socket sqe
@@ -152,10 +155,13 @@ int main(int argc, char *argv[]) {
                     add_socket_write(&ring, conn_i.fd, bid, bytes_read, 0);
                 }
             } else if (type == WRITE) {
+                // write has been completed, first re-add the buffer
                 add_provide_buf(&ring, conn_i.bid, group_id);
+                // add a new read for the existing connection
                 add_socket_read(&ring, conn_i.fd, group_id, MAX_MESSAGE_LEN, IOSQE_BUFFER_SELECT);
             }
         }
+
         io_uring_cq_advance(&ring, count);
     }
 }
